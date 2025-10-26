c3a8a673684eed82897ee8d6721e1fd330bb800a
isaacmatovu <matovuisaac646@gmail.com>
2025-10-26 14:27:14 +0300

    added react helmet wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { HelmetProvider } from "react-helmet-async";
 
 // Ensure the DOM is fully loaded before rendering
 const renderApp = () => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </StrictMode>
   );
 };
